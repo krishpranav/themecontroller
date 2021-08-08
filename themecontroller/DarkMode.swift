5aa8a6cc738fd26781debe66741d4ea1b9ec8413
krishpranav <krisna.pranav@gmail.com>
2021-08-08 17:46:57 +0530

    DarkMode

---
+++ themecontroller/DarkMode.swift
@@ -0,0 +1,30 @@
+//
+//  DarkMode.swift
+//  themecontroller
+//
+//  Created by Elangovan Ayyasamy on 08/08/21.
+//
+
+import AppKit
+
+struct DarkMode {
+    private static let prefix = "tell application \"System Events\" to tell appearance preferences to"
+
+    static var isEnabled: Bool {
+        get {
+            if #available(macOS 10.14, *) {
+                return NSAppearance.current.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+            } else {
+                return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+            }
+        }
+        set {
+            toggle(force: newValue)
+        }
+    }
+
+    static func toggle(force: Bool? = nil) {
+        let value = force.map(String.init) ?? "not dark mode"
+        runAppleScript("\(prefix) set dark mode to \(value)")
+    }
+}
