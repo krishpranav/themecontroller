4aad801d6fc17d425418fa01f29bf21418207e09
krishpranav <krisna.pranav@gmail.com>
2021-08-08 17:49:36 +0530

    themecontroller: func showUsage

--- themecontroller/main.swift
+++ themecontroller/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+let VERSION = "1.0.0"
+
+func showUsage() {
+    print(
+        """
+          Usage
+            $ themecontroller [command]
+          Commands
+            <none>  Toggle dark mode
+            on      Enable dark mode
+            off     Disable dark mode
+            status  Dark mode status
+          Created by krishpranav
+        """
+    )
+}
+
 
